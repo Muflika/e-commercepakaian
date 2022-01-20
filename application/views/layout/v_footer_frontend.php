33c3645a533f0c162d7d8f17898426ee3468cebc
Muflika rahmadani <50790174+Muflika@users.noreply.github.com>
2022-01-21 05:16:51 +0700

    Add files via upload

---
+++ application/views/layout/v_footer_frontend.php
@@ -0,0 +1,32 @@
+</div>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+
+<!-- /.control-sidebar -->
+
+<!-- Main Footer -->
+<footer class="main-footer">
+	<!-- To the right -->
+	<div class="float-right d-none d-sm-inline">
+		
+	</div>
+	<!-- Default to the left -->
+	<strong>Copyright &copy; 2021-2022 <a href="">Muflika Rahmadani</a>.</strong> 
+</footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<script>
+	window.setTimeout(function() {
+		$(".alert").fadeTo(500, 0).slideUp(500, function() {
+			$(this).remove();
+		});
+	}, 3000)
+</script>
+</body>
+
+</html>
